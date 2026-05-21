1b0639046839d6ec7d3374f7194f7fa4ec1965c7
Dimas nugroho <144247337+dimas1625@users.noreply.github.com>
2026-05-21 13:51:48 +0700

    fix: code error

--- app.test.js
+++ app.test.js
@@ -1,5 +1,5 @@
 const { greet } = require('./index');
 
 test('fungsi greet mengembalikan sapaan yang benar', () => {
-  expect(greet('Dunia')).toBe('Halo, Dunia! Selamat datang di CI/CD.');
+  expect(greet('Dunia')).toBe('Halo, SALAH! coba lagi.');
 });
